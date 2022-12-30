f3030099e0f3479a3374c19d3cb2db2572c170a8
tanjie <tanjie@gsafety.com>
2022-12-30 13:50:34 +0800

    fix: 代码格式化使用prettier插件  eslint作为代码规范工具

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -8,45 +8,47 @@
 </template>
 <script lang="ts">
 /* eslint-disable */
-import { useAppStore } from "../store/modules/app";
-import bus from "vue3-eventbus";
-import { storeToRefs } from "pinia";
+import { useAppStore } from '../store/modules/app';
+import bus from 'vue3-eventbus';
+import { storeToRefs } from 'pinia';
 
 export default defineComponent({
-  name: "dashboard",
-  components: {},
-  setup() {
-    const router = useRouter();
-    const appStore = useAppStore();
-    const { appTitle } = storeToRefs(appStore);
+	name: 'dashboard',
+	components: {},
+	setup() {
+		const router = useRouter();
+		const appStore = useAppStore();
+		const { appTitle } = storeToRefs(appStore);
 
-    onMounted(() => {
-      appStore.appTitle = appStore.getAppTitle;
-      bus.on("titilChage", (item) => {
-        appStore.appTitle = item;
-      });
-    });
-    const home = () => {
-      appStore.appTitle = appStore.getConfig.TITLE;
-      router.push({ path: "/dashboard/home" });
-    };
-    return { home, appTitle };
-  },
+		onMounted(() => {
+			appStore.appTitle = appStore.getAppTitle;
+			bus.on('titilChage', (item: any) => {
+				appStore.appTitle = item;
+			});
+		});
+		const home = () => {
+			appStore.appTitle = appStore.getConfig.TITLE;
+			router.push({ path: '/dashboard/home' });
+		};
+		return { home, appTitle };
+	},
 });
 </script>
 
 <style scoped>
 .layout {
-  max-width: 3840px;
-  max-height: 1080px;
-  overflow-x: hidden;
-  overflow-y: hidden;
+	max-width: 3840px;
+	max-height: 1080px;
+	overflow-x: hidden;
+	overflow-y: hidden;
 }
+
 .header {
-  height: 10%;
-  font-size: 40px;
+	height: 10%;
+	font-size: 40px;
 }
+
 .content {
-  height: 90%;
+	height: 90%;
 }
 </style>
--- src/views/about/AboutView.vue
+++ src/views/about/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <iframe
-    :src="frameSrc"
     id="frames"
+    :src="frameSrc"
     frameborder="0"
     sandbox="allow-forms allow-scripts allow-same-origin allow-popups"
     width="100%"
@@ -12,27 +12,27 @@
 <script lang="ts">
 /* eslint-disable */
 export default defineComponent({
-  name: "detail",
-  components: {},
-  setup() {
-    const frameSrc = ref();
-    const router = useRouter();
-    const currentRouteInfo = router.currentRoute.value;
-    onMounted(() => {
-      console.log(currentRouteInfo.query);
-      frameSrc.value = currentRouteInfo.query.url;
-      // frameSrc.value = "http://localhost:8093";
-      // frameSrc.value = "https://workspace.easyv.cloud/bigscreen/1276169";
-    });
+	name: 'detail',
+	components: {},
+	setup() {
+		const frameSrc = ref();
+		const router = useRouter();
+		const currentRouteInfo = router.currentRoute.value;
+		onMounted(() => {
+			console.log(currentRouteInfo.query);
+			frameSrc.value = currentRouteInfo.query.url;
+			// frameSrc.value = 'http://localhost:8093';
+			// frameSrc.value = 'https://workspace.easyv.cloud/bigscreen/1276169';
+		});
 
-    function hideLoading() {}
-    return { frameSrc, hideLoading };
-  },
+		function hideLoading() {}
+		return { frameSrc, hideLoading };
+	},
 });
 </script>
 <style scoped>
 .about {
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 }
 </style>
