929841ec0f40f7f7002f9823acacc89142ed386c
tanjie <tanjie@gsafety.com>
2023-02-01 17:18:02 +0800

    add: 添加easyv内部访问自己的业务服务传递token

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="home" style="height: 100%; width: 100%">
-    <iframe
-      :src="frameSrc"
-      id="frames"
-      frameborder="0"
-      sandbox="allow-forms allow-scripts allow-same-origin allow-popups"
-      width="100%"
-      height="100%"
-      @load="hideLoading"
-    ></iframe>
+    <iframe :src="frameSrc" id="frames" frameborder="0"
+      sandbox="allow-forms allow-scripts allow-same-origin allow-popups" width="100%" height="100%"
+      @load="hideLoading"></iframe>
   </div>
 </template>
 
@@ -30,16 +24,16 @@ export default defineComponent({
     onMounted(() => {
       if (isFirst) {
         const info: any = appStore.applink;
-        frameSrc.value = info[0].url;
+        frameSrc.value = info[0].url + '#token=Bearer ' + appStore.getToken;
       }
       // 系统切换
       bus.on("SystemUrl", (ele: any) => {
-        frameSrc.value = ele;
+        frameSrc.value = ele + '#token=Bearer ' + appStore.getToken;
         isFirst.value = false;
       });
     });
 
-    function hideLoading() {}
+    function hideLoading() { }
 
     return { frameSrc, hideLoading };
   },
