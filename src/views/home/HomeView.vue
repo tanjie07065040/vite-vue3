70cdcc7a576f6121ea74f7a78af204836fc7ca8f
tanjie <tanjie@gsafety.com>
2023-02-17 19:37:53 +0800

    fix: Iframe内部不能点击的问题

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="home" style="height: 100%; width: 100%">
-    <iframe :src="frameSrc" id="frames" frameborder="0"
-      sandbox="allow-forms allow-scripts allow-same-origin allow-popups" width="100%" height="100%"
-      @load="hideLoading"></iframe>
+    <iframe
+      :src="frameSrc"
+      style="pointer-events: unset;"
+      id="frames"
+      frameborder="0"
+      sandbox="allow-forms allow-scripts allow-same-origin allow-popups allow-top-navigation allow-downloads"
+      width="100%"
+      height="100%"
+      @load="hideLoading"
+    ></iframe>
   </div>
 </template>
 
@@ -24,16 +31,16 @@ export default defineComponent({
     onMounted(() => {
       if (isFirst) {
         const info: any = appStore.applink;
-        frameSrc.value = info[0].url + '#token=Bearer ' + appStore.getToken;
+        frameSrc.value = info[0].url + "#token=Bearer " + appStore.getToken;
       }
       // 系统切换
       bus.on("SystemUrl", (ele: any) => {
-        frameSrc.value = ele + '#token=Bearer ' + appStore.getToken;
+        frameSrc.value = ele + "#token=Bearer " + appStore.getToken;
         isFirst.value = false;
       });
     });
 
-    function hideLoading() { }
+    function hideLoading() {}
 
     return { frameSrc, hideLoading };
   },
